2b5d798ec60cdac7504f7a74c42c4b7192283abc
Marc-Antoine Ruel <maruel@gmail.com>
2021-02-09 08:50:37 -0500

    Initial v0

---
+++ gomoudlesversioningtest.go
@@ -0,0 +1,6 @@
+// Package gomodulesversioningtest exists to test behavior of go modules
+// versioning.
+package gomodulesversioningtest
+
+// AnswerToLife is the answer to life and everything.
+const AnswerToLife = 7
